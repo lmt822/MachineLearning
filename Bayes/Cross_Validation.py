90a559982ccf25d044a2e550c865a90ebb469c4e
Mengtian Li <mengtianli@wr-130-64-179-85.medford.tufts.edu>
2015-10-13 01:26:10 -0400

    finished implementation for both X validation and naive bayes

---
+++ Bayes/Cross_Validation.py
@@ -0,0 +1,257 @@
+# Mengtian Li
+# Comp135 a2
+# Oct 10 2015
+import sys
+import os
+import re
+import math
+import random
+from operator import itemgetter
+
+# read index from file either short or full
+def read_index(file_path, index_list):
+	with open(file_path) as f:
+		for line in f:
+			index_list.append(int(line))
+
+# Read CLEAN files from given data_path and given index, then stores
+# the content of each file to a file_list. Each index correspond to 
+# the file name
+def read_files(data_path, data_index):
+	file_list = [None] * int(len(os.listdir(data_path)))	
+	for entry in os.listdir(data_path):
+		if entry.endswith("clean"):
+			index = int(re.findall('\d+',entry)[0])
+			if index in data_index:
+				path = os.path.join(data_path, entry)
+				with open(path, 'r') as f:
+					temp = f.read()
+					file_list[index] = temp
+	return file_list
+
+# Read a given file that contains the outcome of each text file
+# store each index to either positive_index list or negative_index
+# list
+def read_class(dir_path, positive_index, negative_index, file_name):
+	path = os.path.join(dir_path, file_name)
+	with open(path) as f:
+		for line in f:
+			line = line.split('|')
+			if line[1].lower().startswith('y'):
+				positive_index.append(int(line[0]))
+			else:
+				negative_index.append(int(line[0]))
+
+# Parse each training data to a list of tokens extract from text file
+def parse_tokens(data_files, index_list, token_list, training_index):
+	temp_list = []
+	for i in index_list:
+		if i in training_index:
+			temp_list.append(data_files[i].split())
+	for element in temp_list:
+		for token in element:
+			token_list.append(token)
+
+# For each token in the token list, count the frequency of each token
+# store each token and corresponding frequency in a vector in a list
+# return the length of the number of distinct tokens
+def calculate_freq(token_list, token_freq):
+	token_seen = []
+	for token in token_list:
+		if token in token_seen:
+			for element in token_freq:
+				if element[0] == token:
+					element[1] += 1
+		else:
+			token_seen.append(token)
+			temp = []
+			temp.append(token)
+			temp.append(int(1))
+			token_freq.append(temp)
+	return len(token_seen)
+
+# Return a data file list with index provided in index_list
+def split_data(data_files, index_list):
+	to_rtn = [None] * len(data_files)
+	for i in index_list:
+		to_rtn[i] = data_files[i]
+	return to_rtn
+
+# Given frequency list and total number of tokens, calculate with
+# probability and smooth with m and vocabulary size
+def calculate_prob(token_freq, token_count, m, v):
+	for element in token_freq:
+		element[1] = float(element[1] + m) / float(token_count + m*v)
+
+# Return mean of a list
+def mean(list):
+	return sum(list) / float(len(list))
+
+# Return sum of square deviations of sequence data.
+def _ss(data):
+    c = mean(data)
+    ss = sum((x-c)**2 for x in data)
+    return ss
+
+# calculate standard deviation for list
+def stdev(data):
+    n = len(data)
+    if n < 2:
+        raise ValueError('variance requires at least two data points')
+    ss = _ss(data)
+    pvar = ss/n # the population variance
+    return pvar**0.5	
+
+# input file should include a file that specifies the index
+# for training set.(Assume all in boundry)
+# argument list:
+# 1: path to data files
+# 2: index list to select
+# 3: m as smoothing factor
+# 4: N as the factor of subsample size
+def main():
+	# Read from file
+	data_files = []
+	
+	positive_index = []
+	negative_index = []
+	index_list = []	
+	k_fold_list = [[] for i in range(2)]
+	read_index(sys.argv[2], index_list)
+	data_files = read_files(sys.argv[1], index_list)
+	m = int(sys.argv[3])
+	N = float(sys.argv[4])
+	# prepare for 10-fold, get two list with positive text and negative
+	# text 
+	read_class(sys.argv[1], positive_index, negative_index, "index.full")
+	for index in index_list:
+		if index in positive_index:
+			k_fold_list[0].append(index)
+		else:
+			k_fold_list[1].append(index)
+	random.shuffle(k_fold_list[0])
+	random.shuffle(k_fold_list[1])
+	# start 10 fold, if not divisble by 10, attach the remainders to 
+	# last fold
+	hold_positive = int(len(k_fold_list[0]) / 10)
+	hold_negative = int(len(k_fold_list[1]) / 10)
+	# attch first half in each fold with positive index and later half
+	# with negative class
+	for i in range(0, 10):
+		tmp_list = []
+		if i != 9:
+			for j in range(0,hold_positive):
+				tmp_list.append(k_fold_list[0].pop(0))
+				j += 1
+			for k in range(0, hold_negative):
+				tmp_list.append(k_fold_list[1].pop(0))
+				k += 1
+		else:
+			for index in k_fold_list[0]:
+				tmp_list.append(index)
+			for index in k_fold_list[1]:
+				tmp_list.append(index)
+		k_fold_list.append(tmp_list)
+		i += 1
+	# pop the first two list that is used to store the index of positive
+	# and negative class
+	k_fold_list.pop(0)
+	k_fold_list.pop(0)
+	
+	# Run the cross validation
+	accuracies = []
+	for i in range(0, 10):
+		training_data = []
+		test_data = []
+		training_index = []
+		test_index = []
+		positive_token_list = []
+		negative_token_list = []
+		positive_token_freq = []
+		negative_token_freq = []
+		test_score = []
+
+		
+		test_index = k_fold_list[i]
+		for k in range(0, 10):
+			if k != i:
+				for element in k_fold_list[k]:
+					training_index.append(element)
+		# shuffle the training data so that each 0.x * N is randomized
+		random.shuffle(training_index)
+		# Select N number of sub sample
+		cut = int(N * len(training_index))
+		training_index = training_index[:cut] 
+		
+		# rest is basically same as naive_bayes.py
+		training_data = split_data(data_files, training_index)
+		test_data = split_data(data_files, test_index)
+		
+		# parse training data
+		parse_tokens(training_data, positive_index, positive_token_list, training_index)
+		parse_tokens(training_data, negative_index, negative_token_list, training_index)
+		# Calculate the possiblity that positive and negative in training data
+		training_size = len(positive_index) + len(negative_index)
+		P_positive = float(len(positive_index)) / float(training_size)
+		P_negative = float(len(negative_index)) / float(training_size)
+		p_token_size = calculate_freq(positive_token_list, positive_token_freq)
+		n_token_size = calculate_freq(negative_token_list, negative_token_freq)
+		vocab_size = p_token_size + n_token_size
+		calculate_prob(positive_token_freq, len(positive_token_list), m, vocab_size)
+		calculate_prob(negative_token_freq, len(negative_token_list), m, vocab_size)
+		# calculate score for each test data
+		for text in test_data:
+			if text == None:
+				continue
+			else:
+				temp_score = []
+				temp_score.append(test_data.index(text))
+				temp_text = text.split()
+				# calculate positive score
+				sum = math.log(P_positive)
+				for token in temp_text:
+					check = 1
+					for element in positive_token_freq:
+						if element[0] == token:
+							sum = sum + math.log(element[1])
+							check = 0
+							break
+					if check:
+						for element in negative_token_freq:
+							if element[0] == token:
+								sum = sum - float("inf")
+				temp_score.append(sum)
+				sum = math.log(P_negative)
+				for token in temp_text:
+					check = 1
+					for element in negative_token_freq:
+						if element[0] == token:
+							sum = sum + math.log(element[1])
+							check = 0
+							break
+					if check:
+						for element in positive_token_freq:
+							if element[0] == token:
+								sum = sum - float("inf")
+				temp_score.append(sum)
+			test_score.append(temp_score)
+		# make prediction on threshold 0.5
+		positive_prediction = []
+		negative_prediction = []
+		for element in test_score:
+			if element[1] > element[2]:
+				positive_prediction.append(element[0])
+			else:
+				negative_prediction.append(element[0])
+		# calculate accuracy
+		num_correct = 0
+		for element in positive_prediction:
+			if element in positive_index:
+				num_correct += 1
+		for element in negative_prediction:
+			if element in negative_index:
+				num_correct += 1
+		accuracy = float(num_correct) / float(len(test_index))
+		accuracies.append(accuracy)
+	print mean(accuracies), stdev(accuracies)
+main()
--- Bayes/naive_bayes.py
+++ Bayes/naive_bayes.py
@@ -12,9 +12,11 @@ def read_index(file_path, index_list):
 	with open(file_path) as f:
 		for line in f:
 			index_list.append(int(line))
-# Read CLEAN files from given data_path and given index, then stores
+
+# Read CLEAN files from given data_path and given index, if the
+# index matches the training or test index given, then stores
 # the content of each file to a file_list. Each index correspond to 
-# the file name
+# the file name. 
 def read_files(data_path, training_index, test_index):
 	file_list = [None] * int(len(os.listdir(data_path)))	
 	for entry in os.listdir(data_path):
@@ -27,6 +29,9 @@ def read_files(data_path, training_index, test_index):
 					file_list[index] = temp
 	return file_list
 
+# Read a given file that contains the outcome of each text file
+# store each index to either positive_index list or negative_index
+# list
 def read_class(dir_path, positive_index, negative_index, file_name):
 	path = os.path.join(dir_path, file_name)
 	with open(path) as f:
@@ -36,6 +41,8 @@ def read_class(dir_path, positive_index, negative_index, file_name):
 				positive_index.append(int(line[0]))
 			else:
 				negative_index.append(int(line[0]))
+
+# Parse each training data to a list of tokens extract from text file
 def parse_tokens(data_files, index_list, token_list, training_index):
 	temp_list = []
 	for i in index_list:
@@ -45,6 +52,9 @@ def parse_tokens(data_files, index_list, token_list, training_index):
 		for token in element:
 			token_list.append(token)
 
+# For each token in the token list, count the frequency of each token
+# store each token and corresponding frequency in a vector in a list
+# return the length of the number of distinct tokens
 def calculate_freq(token_list, token_freq):
 	token_seen = []
 	for token in token_list:
@@ -58,19 +68,27 @@ def calculate_freq(token_list, token_freq):
 			temp.append(token)
 			temp.append(int(1))
 			token_freq.append(temp)
+	return len(token_seen)
 
+# Return a data file list with index provided in index_list
 def split_data(data_files, index_list):
 	to_rtn = [None] * len(data_files)
 	for i in index_list:
 		to_rtn[i] = data_files[i]
 	return to_rtn
-
+# Given frequency list and total number of tokens, calculate with
+# probability and smooth with m and vocabulary size
 def calculate_prob(token_freq, token_count, m, v):
 	for element in token_freq:
 		element[1] = float(element[1] + m) / float(token_count + m*v)
 
 # input file should include a file that specifies the index
 # for training set.(Assume all in boundry)
+# argument list:
+# 1: path to data files
+# 2: training index file
+# 3: test index file
+# 4: m as factor for smoothing
 def main():
 	# Read from file
 	data_files = []
@@ -91,7 +109,7 @@ def main():
 	# split training and test data
 	training_data = split_data(data_files, training_index)
 	test_data = split_data(data_files, test_index)
-	# short
+	# short or full
 	read_class(sys.argv[1], positive_index, negative_index, "index.short")
 	m = int(sys.argv[4])
 	# parse training data
@@ -103,9 +121,10 @@ def main():
 	P_negative = float(len(negative_index)) / float(training_size)
 	calculate_freq(positive_token_list, positive_token_freq)
 	calculate_freq(negative_token_list, negative_token_freq)
-	vocab_size = len(positive_token_list) + len(negative_token_list)
-	calculate_prob(positive_token_freq, len(positive_token_list), m, vocab_size)
-	calculate_prob(negative_token_freq, len(negative_token_list), m, vocab_size)
+	# token_size used to calculate vocab size
+	p_token_size = calculate_freq(positive_token_list, positive_token_freq)
+	n_token_size = calculate_freq(negative_token_list, negative_token_freq)
+	vocab_size = p_token_size + n_token_size
 	# calculate score for each test data
 	for text in test_data:
 		if text == None:
@@ -115,8 +134,12 @@ def main():
 			temp_score.append(test_data.index(text))
 			temp_text = text.split()
 			# calculate positive score
+			# Use the sum of log(P) to avoid underflow
 			sum = math.log(P_positive)
 			for token in temp_text:
+				# check if a token has been seen in current class
+				# if not seen in current class but seen in the other
+				# record 0 count for that token
 				check = 1
 				for element in positive_token_freq:
 					if element[0] == token:
